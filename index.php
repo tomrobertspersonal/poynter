c656cc8a994b1386898b47dd82dd612837554c2f
tomrobertspersonal <to_mroberts@hotmail.com>
2015-08-26 19:43:43 +0100

    Added Post Headers

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php get_header(); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
+		<header>
+			<button id="open-cover">
+				<i class="fa fa-angle-right"></i>
+			</button>
+		</header>	
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,2 +1,13 @@
 <?php get_header(); ?>
+<?php get_header(); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
+		<header>
+			<button id="open-cover">
+				<i class="fa fa-angle-right"></i>
+			</button>
+		</header>
+		<article>
+			<?php the_content(); ?>
+		</article>	
+<?php get_footer(); ?>
 <?php get_footer(); ?>
