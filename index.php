c85ca875df6727af96100bb307ff664e10c8d39f
tomrobertspersonal <to_mroberts@hotmail.com>
2015-08-26 22:34:32 +0100

    Minor

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 	<div class="frame">
 	<?php //if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
 		<ul class="slidee">
-        	<li>Test</li>
-        	<li>Tes 2</li>
-        	<li>Tes 3</li>
+        	<li style="background-color: red;">Test</li>
+        	<li style="background-color: geen;">Tes 2</li>
+        	<li style="background-color: blue;">Tes 3</li>
     	</ul>
 	<?php // endwhile; endif; ?>
 	</div>
