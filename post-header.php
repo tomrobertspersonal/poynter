ddce0f3645946c9275708b9cca02ed12e4d71404
tomrobertspersonal <to_mroberts@hotmail.com>
2015-08-26 20:27:37 +0100

    Minor

--- post-header.php
+++ post-header.php
@@ -1,5 +1,5 @@
 		<header>
-			<h1><?php the_title(); ?></h1>
+			<h1><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
 			<button id="open-cover">
 				<i class="fa fa-angle-right"></i>
 			</button>
